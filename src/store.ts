200aa7c96c9efad467c437812d7dec95e78c110f
Rourou <mimikapple@gmail.com>
2019-04-26 19:00:42 +0800

    update

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex, { Commit, Dispatch } from 'vuex'
 import member from '@/store/member'
+import error from '@/store/member'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
       member,
+      error,
   }
 })
 
