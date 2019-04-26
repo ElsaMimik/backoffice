0680b6dfddf75e027d17a32a5ac2eae2755820a0
Rourou <mimikapple@gmail.com>
2019-04-26 18:35:09 +0800

    update

---
+++ src/store/error.ts
@@ -0,0 +1,24 @@
+import { ActionContextBasic } from '@/store'
+const initState: State = {
+}
+
+
+
+export interface State {
+}
+
+const getters = {
+}
+
+const actions = {
+}
+
+const mutations = {
+}
+
+export default {
+    state: initState,
+    getters,
+    actions,
+    mutations,
+}
