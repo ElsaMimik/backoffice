328dedab9b14896b25e4c1c669affb34eed96507
Rourou <mimikapple@gmail.com>
2019-04-29 10:14:46 +0800

    update

---
+++ src/models/status/member.ts
@@ -0,0 +1,21 @@
+export const MemberStatus = Object.freeze({
+    // 正常
+    Normal: 'Normal',
+    //不可提現
+    E1: 'E1',
+    // 不可登入
+    E2: 'E2'
+  });
+
+  export const LoginType = Object.freeze({
+    Mobile: 'Mobile',
+    Web: 'Web'
+  });
+
+  export const RiskControllLevel = Object.freeze({
+    A: 'A',
+    B: 'B',
+    C: 'C',
+    D: 'D',
+    W: 'W'
+  });
