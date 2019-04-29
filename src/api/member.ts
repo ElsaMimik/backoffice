50521092c00af60e70a84f46a466d3cfd8dd59ce
Rourou <mimikapple@gmail.com>
2019-04-29 10:29:01 +0800

    update

--- src/api/member.ts
+++ src/api/member.ts
@@ -1,14 +1,15 @@
 import * as Model from '@/models/interfaces/member'
+import { MemberStatus } from '@/models/status/member'
 
 export default {
     getMember(id: string): Model.IMemberResponse {
-        const data: Model.IMemberResponse = { uuid: 'employee_01', name: 'Rourou', };
+        const data: Model.IMemberResponse = { uuid: 'employee_01', name: 'Rourou', status: MemberStatus.E1 };
         return <Model.IMemberResponse>(data);
     },
     getMemberList(id: string): Model.IMemberResponse[] {
         const data: Model.IMemberResponse[] = [
-            { uuid: 'employee_01', name: 'Rourou', },
-            { uuid: 'employee_02', name: 'Ellen', },
+            { uuid: 'employee_01', name: 'Rourou', status: MemberStatus.E1 },
+            { uuid: 'employee_02', name: 'Ellen', status: MemberStatus.E2 },
         ];
         return <Model.IMemberResponse[]>(data);
     },
