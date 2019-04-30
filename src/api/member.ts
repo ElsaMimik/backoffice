a9e05711f72a7a25ec1474124310ba6ebb3c6890
Rourou <mimikapple@gmail.com>
2019-04-30 17:45:02 +0800

    update

--- src/api/member.ts
+++ src/api/member.ts
@@ -1,12 +1,22 @@
 import * as Model from '@/models/interfaces/member'
 import { MemberStatus } from '@/models/status/member'
+import handler from '@/utilities/api-handler'
 
 export default {
     getMember(id: string): Model.IMemberResponse {
         const data: Model.IMemberResponse = { uuid: 'employee_01', name: 'Rourou', status: MemberStatus.E1 };
         return <Model.IMemberResponse>(data);
     },
-    getMemberList(id: string): Model.IMemberResponse[] {
+    async getMemberList(id: string): Promise<Model.IMemberResponse[]> {
+
+        console.log('test')
+        let config = {
+            url: '',
+            method: 'get'
+        };
+        let response = await handler.request(config);
+        console.log(response)
+
         const data: Model.IMemberResponse[] = [
             { uuid: 'employee_01', name: 'Rourou', status: MemberStatus.E1 },
             { uuid: 'employee_02', name: 'Ellen', status: MemberStatus.E2 },
--- src/utilities/api-handler.ts
+++ src/utilities/api-handler.ts
@@ -1,14 +1,46 @@
-import axios, { AxiosRequestConfig } from 'axios'
+import axios, { AxiosInstance, AxiosResponse, AxiosRequestConfig, AxiosError } from 'axios'
 
-export default class HttpHandler {
-  baseUrl: string;
-  constructor(baseUrl: string) {
-    this.baseUrl = baseUrl;
-  }
-  async request(config: AxiosRequestConfig): Promise<any> {
-    let result = await axios.create({
-      baseURL: this.baseUrl
-    }).request(config);
-    return result;
-  }
-};
+const baseURL = 'https://www.thef2e.com/api/tagList';
+
+class HttpModel {
+
+    constructor() {
+        axios.defaults.baseURL = baseURL;
+    }
+
+    async request(config: AxiosRequestConfig): Promise<AxiosResponse> {
+        let result: AxiosResponse = await axios.create().request(config);
+        console.log(result.status)
+        return result.data;
+    }
+}
+
+const model = new HttpModel();
+
+export default model;
+
+
+// import axios, { AxiosRequestConfig } from 'axios'
+// import { IError } from '@/models/interfaces/error'
+// export default class HttpHandler {
+//   baseUrl: string;
+//   constructor(baseUrl: string) {
+//     this.baseUrl = baseUrl;
+//   }
+//   async request<T>(config: AxiosRequestConfig, responseType: T): Promise<T | IError> {
+//     let result = await axios.create({
+//       baseURL: this.baseUrl
+//     }).request(config);
+//     if (result.status === 200) {
+//       return result.data;
+//     }
+//     else {
+//       const err: IError = {
+//         code: '',
+//         message: 'string',
+//         traceId: 'string',
+//       };
+//       return err;
+//     }
+//   }
+// };
